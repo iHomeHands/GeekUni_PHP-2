ecb82fce1edc09712567f717662dd719f152fe40
Sergey Fedorov <d946@mail.ru>
2017-07-17 21:14:36 +0300

    Добавление классов App и Config

---
+++ lib/App.class.php
@@ -0,0 +1,10 @@
+<?php
+
+class App 
+{
+    public static function Init() 
+    {
+        date_default_timezone_set('Europe/Moscow');
+        db::getInstance()->Connect(Config::get('db_user'), Config::get('db_password'), Config::get('db_base'));
+    }
+}
