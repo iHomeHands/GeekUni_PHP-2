e903d905f728b19b888fba689b4fce3a011cf798
Sergey Fedorov <d946@mail.ru>
2017-07-17 21:11:14 +0300

    Добавление файлов конфигураций

---
+++ config.default.php
@@ -0,0 +1,17 @@
+<?php
+$config['db_user'] = 'root';
+$config['db_password'] = '';
+$config['db_base'] = 'shop';
+$config['db_host'] = 'localhost';
+$config['db_charset'] = 'UTF-8';
+
+$config['path_root'] = __DIR__;
+$config['path_public'] = $config['path_root'] . '/public';
+$config['path_model'] = $config['path_root'] . '/model';
+$config['path_controller'] = $config['path_root'] . '/controller';
+$config['path_cache'] = $config['path_root'] . '/cache';
+$config['path_data'] = $config['path_root'] . '/data';
+$config['path_fixtures'] = $config['path_data'] . '/fixtures';
+$config['path_commands'] = $config['path_root'] . '/lib/commands';
+
+$config['path_logs'] = $config['path_root'] . '/logs';
