df66f60a8ac8a42efa5c6c0413ceea05c1369067
Sergey Fedorov <d946@mail.ru>
2017-07-22 09:06:44 +0300

    Изменение класса App

--- lib/App.class.php
+++ lib/App.class.php
@@ -6,5 +6,16 @@ class App
     {
         date_default_timezone_set('Europe/Moscow');
         db::getInstance()->Connect(Config::get('db_user'), Config::get('db_password'), Config::get('db_base'));
+
+        if (php_sapi_name() !== 'cli' && isset($_SERVER) && isset($_GET)) {
+            self::web(isset($_GET['url']) ? $_GET['url'] : '');
+        }
     }
+
+    protected static function web($url)
+    {
+        echo $url;
+    }
+
+
 }
