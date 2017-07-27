0948952d7bd8c1d7343b555c22bf7065015e3b1f
Sergey Fedorov <d946@mail.ru>
2017-07-27 20:06:38 +0300

    Обновление фикстур

--- data/fixtures/100_pages.sql
+++ data/fixtures/100_pages.sql
@@ -1,9 +1,4 @@
-DROP TABLE IF EXISTS pages;
-CREATE TABLE pages (
-     id INT NOT NULL AUTO_INCREMENT,
-     name VARCHAR(256) NOT NULL,
-     PRIMARY KEY (id)
-);
+TRUNCATE TABLE pages;
 INSERT INTO pages (`name`) VALUES ('Главная');
 INSERT INTO pages (`name`) VALUES ('О Магазине');
 INSERT INTO pages (`name`) VALUES ('Каталог');
