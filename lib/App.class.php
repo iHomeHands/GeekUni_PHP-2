b884a41947cbe454ed054a75e067b74f69e02a6f
Sergey Fedorov <d946@mail.ru>
2017-07-27 20:00:34 +0300

    Добавление обработки ajax

--- lib/App.class.php
+++ lib/App.class.php
@@ -8,12 +8,27 @@ class App
         db::getInstance()->Connect(Config::get('db_user'), Config::get('db_password'), Config::get('db_base'));
 
         if (php_sapi_name() !== 'cli' && isset($_SERVER) && isset($_GET)) {
-            self::web(isset($_GET['url']) ? $_GET['url'] : '');
+            self::web(isset($_GET['query_string']) ? $_GET['query_string'] : '');
         }
     }
 
     protected static function web($url)
     {
+        $url = explode('/', $url);
+        if (isset($url[1])) {
+            $_GET['page'] = $url[1];
+            if (isset($url[2])) {
+                if (is_numeric($url[2])) {
+                    $_GET['id'] = $url[2];
+                } else {
+                    $_GET['action'] = $url[2];
+                }
+                if (isset($url[3])) {
+                    $_GET['id'] = $url[3];
+                }
+            }
+        }
+
         if (isset($_GET['page'])) {
             $controllerName = $_GET['page'] . 'Controller';
             $methodName = isset($_GET['action']) ? $_GET['action'] : 'index';
@@ -21,14 +36,18 @@ class App
             $data = $controller->$methodName($_REQUEST);
 
             $view = $controller->view . '/' . $methodName . '.html';
-
-            require_once (Config::get('path_libs') . '/smarty/Autoloader.php');
-            Smarty_Autoloader::register();
-            $smarty = new Smarty();
-            $smarty->setTemplateDir(Config::get('path_templates'));
-            $smarty->assign('title', $controller->title);
-            $smarty->assign('data', $data);
-            echo $smarty->fetch($view);
+            if (!isset($_GET['asAjax'])) {
+                require_once (Config::get('path_libs') . '/smarty/Autoloader.php');
+                Smarty_Autoloader::register();
+                $smarty = new Smarty();
+                $smarty->setTemplateDir(Config::get('path_templates'));
+                $smarty->assign('title', $controller->title);
+                $smarty->assign('data', $data);
+                $smarty->assign('categories', CategoriesController::getCategories(0));
+                echo $smarty->fetch($view);
+            } else {
+                echo json_encode($data);
+            }
         }
     }
 
