451b90f524018621c4ea74016f287971a9aa974f
Fedorov Sergey <d946@mail.ru>
2017-07-28 08:01:54 +0300

    Добавление файлов в контроль версий

---
+++ controller/categoriesController.class.php
@@ -0,0 +1,29 @@
+<?php
+class CategoriesController extends Controller
+{
+
+    public $view = 'categories';
+
+    public function index($data)
+    {
+        $categories = self::getCategories(isset($data['id']) ? $data['id'] : 0);
+        $goods = self::getGoods(isset($data['id']) ? $data['id'] : 0);
+        return ['subcategories' => $categories, 'goods' => $goods];
+    }
+
+    public static function getCategories($parentId = 0)
+    {
+        return db::getInstance()->Select(
+            'SELECT id, name FROM categories WHERE status=:status AND parent_id = :parent_id',
+            ['status' => Status::Active, 'parent_id' => $parentId]);
+
+    }
+
+    protected function getGoods($categoryId)
+    {
+        return db::getInstance()->Select(
+            'SELECT id, name, price FROM goods WHERE category = :category AND status=:status',
+            ['status' => Status::Active, 'category' => $categoryId]);
+    }
+}
+?>
