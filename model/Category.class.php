c3c0648343cbb7f0a77d9d8232eaa9aca67a1f57
Fedorov Sergey <d946@mail.ru>
2017-07-28 08:00:32 +0300

    Добавление моделей

---
+++ model/Category.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class Category extends Model {
+    protected static $table = 'categories';
+
+    protected static function setProperties()
+    {
+        self::$properties['name'] = [
+            'type' => 'varchar',
+            'size' => 512
+        ];
+
+        self::$properties['parent_id'] = [
+            'type' => 'int',
+        ];
+    }
+}
---
+++ model/Good.class.php
@@ -0,0 +1,25 @@
+<?php
+
+class Good extends Model {
+    protected static $table = 'goods';
+
+    protected static function setProperties()
+    {
+        self::$properties['name'] = [
+            'type' => 'varchar',
+            'size' => 512
+        ];
+
+        self::$properties['price'] = [
+            'type' => 'float'
+        ];
+
+        self::$properties['description'] = [
+            'type' => 'text'
+        ];
+
+        self::$properties['category'] = [
+            'type' => 'int'
+        ];
+    }
+}
