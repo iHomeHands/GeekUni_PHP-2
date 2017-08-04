e394f630508cb53dccd173e851cac5e03589dc87
Fedorov Sergey <d946@mail.ru>
2017-08-04 16:07:18 +0300

    Добавлен контроллер Catalog

---
+++ controller/CatalogController.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class CatalogController
+{
+    public function actionIndex()
+    {
+        $categories = Category::getCategoriesList();
+        $latestProducts = Good::getLatestProducts(12);
+
+        require_once(Config::get('path_views') . '/catalog/index.php');
+        return true ;
+    }
+}
