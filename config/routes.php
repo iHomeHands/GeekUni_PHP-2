441350efc0dcd9b73c18be46d8b815be5891a7ce
Fedorov Sergey <d946@mail.ru>
2017-08-04 16:06:59 +0300

    Обновление Route

--- config/routes.php
+++ config/routes.php
@@ -3,8 +3,18 @@
 return array(
     // Product
     'product/([0-9]+)' => 'product/view/$1',
-    // Admin
 
+    'catalog' => 'catalog/index',
+    // Category
+    'category/([0-9]+)/page-([0-9]+)' => 'catalog/category/$1/$2',
+    'category/([0-9]+)' => 'catalog/category/$1',
+    // Cart
+    'cart/checkout' => 'cart/checkout',
+    'cart/delete/([0-9]+)' => 'cart/delete/$1',
+    'cart/add/([0-9]+)' => 'cart/add/$1',
+    'cart/addAjax/([0-9]+)' => 'cart/addAjax/$1',
+    'cart' => 'cart/index',
+    // Admin
     'admin/product/create' => 'adminProduct/create',
     'admin/product/update/([0-9]+)' => 'adminProduct/update/$1',
     'admin/product/delete/([0-9]+)' => 'adminProduct/delete/$1',
