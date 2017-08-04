1908a24ac83fb46abd86bd93688e9697dd08c9f1
Fedorov Sergey <d946@mail.ru>
2017-08-04 07:59:09 +0300

    Добавление Views

---
+++ views/admin_order/index.php
@@ -0,0 +1,7 @@
+<?php include Config::get('path_views') . '/layouts/header.php'; ?>
+
+<section>
+    AdminOrder :: Index
+</section>
+
+<?php include Config::get('path_views') . '/layouts/footer.php'; ?>
---
+++ views/product/view.php
@@ -0,0 +1,7 @@
+<?php include Config::get('path_views') . '/layouts/header.php'; ?>
+
+<section>
+    Product :: View
+</section>
+
+<?php include Config::get('path_views') . '/layouts/footer.php'; ?>
