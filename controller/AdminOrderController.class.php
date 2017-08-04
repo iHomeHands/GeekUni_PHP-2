f90a29eeb7d8be45314f59f32a2d8ce0cffc0a98
Sergey Fedorov <d946@mail.ru>
2017-08-04 20:47:24 +0300

    Обновление контролеров

--- controller/AdminOrderController.class.php
+++ controller/AdminOrderController.class.php
@@ -15,15 +15,51 @@ class AdminOrderController
         return true;
     }
 
-    public function actionView()
+    public function actionView($id)
     {
+        $order = Order::getOrderById($id);
+
+        $productsQuantity = json_decode($order['products'], true);
+
+        $productsIds = array_keys($productsQuantity);
+
+        $products = Good::getProdustsByIds($productsIds);
+
         require_once(Config::get('path_views') . '/admin_order/view.php');
+
         return true;
     }
 
-    public function actionDelete()
+    public function actionUpdate($id)
     {
+        $order = Order::getOrderById($id);
+
+        if (isset($_POST['submit'])) {
+            $userName = $_POST['userName'];
+            $userPhone = $_POST['userPhone'];
+            $userComment = $_POST['userComment'];
+            $date = $_POST['date'];
+            $status = $_POST['status'];
+
+            Order::updateOrderById($id, $userName, $userPhone, $userComment, $date, $status);
+
+            header("Location: /admin/order/view/$id");
+        }
+
+        require_once(Config::get('path_views') . '/admin_order/update.php');
+        return true;
+    }
+
+    public function actionDelete($id)
+    {
+        if (isset($_POST['submit'])) {
+            Order::deleteOrderById($id);
+
+            header("Location: /admin/order");
+        }
+
         require_once(Config::get('path_views') . '/admin_order/delete.php');
+
         return true;
     }
 }
--- controller/CartController.class.php
+++ controller/CartController.class.php
@@ -38,4 +38,68 @@ class CartController
 
         header("Location: /cart");
     }
+
+    public function actionCheckout()
+    {
+        $productsInCart = Cart::getProducts();
+
+        if ($productsInCart == false) {
+            header("Location: /");
+        }
+
+        $categories = Category::getCategoriesList();
+
+        $productsIds = array_keys($productsInCart);
+        $products = Good::getProdustsByIds($productsIds);
+        $totalPrice = Cart::getTotalPrice($products);
+
+        $totalQuantity = Cart::countItems();
+
+        $userName = false;
+        $userPhone = false;
+        $userComment = false;
+
+        $result = false;
+        /*
+        if (!User::isGuest()) {
+            $userId = User::checkLogged();
+            $user = User::getUserById($userId);
+            $userName = $user['name'];
+        } else {
+            $userId = false;
+        }*/
+
+        if (isset($_POST['submit'])) {
+            $userName = $_POST['userName'];
+            $userPhone = $_POST['userPhone'];
+            $userComment = $_POST['userComment'];
+
+            $errors = false;
+            /*
+            if (!User::checkName($userName)) {
+                $errors[] = 'Неправильное имя';
+            }
+            if (!User::checkPhone($userPhone)) {
+                $errors[] = 'Неправильный телефон';
+            }*/
+
+
+            if ($errors == false) {
+                $result = Order::createOrder($userName, $userPhone, $userComment, $userId, $productsInCart);
+
+                if ($result) {
+                    /*
+                    $adminEmail = 'php.start@mail.ru';
+                    $message = '<a href="http://digital-mafia.net/admin/orders">Список заказов</a>';
+                    $subject = 'Новый заказ!';
+                    mail($adminEmail, $subject, $message);
+                    */
+                    Cart::clear();
+                }
+            }
+        }
+
+        require_once(Config::get('path_views') . '/cart/checkout.php');
+        return true;
+    }
 }
