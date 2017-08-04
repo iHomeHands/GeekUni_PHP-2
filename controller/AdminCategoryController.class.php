427e6218d118e602b83346c5e6e7fda61ddd8c28
Fedorov Sergey <d946@mail.ru>
2017-08-04 16:33:14 +0300

    Убрал неиспользуемый код

--- controller/AdminCategoryController.class.php
+++ controller/AdminCategoryController.class.php
@@ -11,29 +11,20 @@ class AdminCategoryController
 
     public function actionCreate()
     {
-        // Обработка формы
         if (isset($_POST['submit'])) {
-            // Если форма отправлена
-            // Получаем данные из формы
             $name = $_POST['name'];
             $sortOrder = $_POST['sort_order'];
             $status = $_POST['status'];
 
-            // Флаг ошибок в форме
             $errors = false;
 
-            // При необходимости можно валидировать значения нужным образом
             if (!isset($name) || empty($name)) {
                 $errors[] = 'Заполните поля';
             }
 
-
             if ($errors == false) {
-                // Если ошибок нет
-                // Добавляем новую категорию
                 Category::createCategory($name, $sortOrder, $status);
 
-                // Перенаправляем пользователя на страницу управлениями категориями
                 header("Location: /admin/category");
             }
         }
@@ -44,40 +35,33 @@ class AdminCategoryController
 
     public function actionUpdate($id)
     {
-
-        // Получаем данные о конкретной категории
         $category = Category::getCategoryById($id);
 
-        // Обработка формы
         if (isset($_POST['submit'])) {
-            // Если форма отправлена
-            // Получаем данные из формы
             $name = $_POST['name'];
             $sortOrder = $_POST['sort_order'];
             $status = $_POST['status'];
 
-            // Сохраняем изменения
             Category::updateCategoryById($id, $name, $sortOrder, $status);
 
-            // Перенаправляем пользователя на страницу управлениями категориями
             header("Location: /admin/category");
         }
+
         require_once(Config::get('path_views') . '/admin_category/update.php');
+
         return true;
     }
 
     public function actionDelete($id)
     {
-        // Обработка формы
         if (isset($_POST['submit'])) {
-            // Если форма отправлена
-            // Удаляем категорию
             Category::deleteCategoryById($id);
 
-            // Перенаправляем пользователя на страницу управлениями товарами
             header("Location: /admin/category");
         }
+
         require_once(Config::get('path_views') . '/admin_category/delete.php');
+
         return true;
     }
 }
